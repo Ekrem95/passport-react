0d504f0ca92a9aaf7011d5c8984f8d5d31435050
Ekremk95 <ekrem3333@gmail.com>
2017-07-28 00:55:55 +0300

    testing progress

--- src/app/Components/Add/Add.spec.js
+++ src/app/Components/Add/Add.spec.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
 import Add from './Add';
 
-const wrapper = shallow(
-  <Add history={[]} />
-);
+const wrapper = mount(<Add history={[]} />);
+
+test('Full Rendering API', () => {
+    sinon.spy(Add.prototype, 'componentWillMount');
+    const wrapper = mount(<Add history={[]} />);
+    expect(Add.prototype.componentWillMount.calledOnce).toEqual(true);
+  });
 
 test('has props', () => {
   expect(wrapper.find('div.add')).toHaveLength(1);
@@ -22,10 +27,42 @@ test('toEqual', () => {
   expect(wrapper.find('[placeholder="Image Source"]')).toEqual(wrapper.find('#src'));
 });
 
-test('click', () => {
-  const title = 'Title';
-  const desc = 'Description';
-  const src = 'Image Source';
+test('check up', () => {
+  const header =  wrapper.find('h1');
+  const input =  wrapper.find('input');
+  const description =  wrapper.find('textarea#desc');
+  const imageSource =  wrapper.find('textarea#src');
+  const button =  wrapper.find('button');
+  const container = wrapper.find('div.add');
 
-  wrapper.find('button').simulate('click');
+  expect(header.text()).toBe('Add');
+
+  expect(input.props().type).toEqual('text');
+  expect(input.props().id).toEqual('title');
+  expect(input.props().placeholder).toEqual('Title');
+
+  expect(description.props().placeholder).toEqual('Description');
+  expect(imageSource.props().placeholder).toEqual('Image Source');
+
+  expect(button.text()).toBe('Add');
+  expect(button.type()).toBe('button');
+
+  // console.log(button.props().onClick);
+
+  expect(wrapper.containsAllMatchingElements([
+    <h1>Add</h1>,
+    <form>
+      <input />
+      <textarea></textarea>
+      <textarea></textarea>
+      <button>Add</button>
+    </form>,
+  ])).toEqual(true);
+});
+
+test('Full Rendering API', () => {
+  expect(wrapper.find('div')).toEqual(wrapper.find('.add'));
+  expect(wrapper.find('[type="text"]')).toEqual(wrapper.find('#title'));
+  expect(wrapper.find('[placeholder="Description"]')).toEqual(wrapper.find('#desc'));
+  expect(wrapper.find('[placeholder="Image Source"]')).toEqual(wrapper.find('#src'));
 });
